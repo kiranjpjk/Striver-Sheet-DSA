5bc654ce6e0b24851e81dd6cc700f4930452bc5c
JOGI VENKATA SAI KIRAN <kiranjogisk123@gmail.com>
2025-07-28 17:11:26 +0530

    Added to a file

--- AtoZ/Arrays/src/MaxConsecutiveOnes485.java
+++ AtoZ/Arrays/src/MaxConsecutiveOnes485.java
@@ -9,13 +9,13 @@ public class MaxConsecutiveOnes485 {
     static int findMaxConsecutiveOnes(int[] nums) {
         int count = 0;
         int maxcount = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if ( nums[i] == 1){
+        for (int num : nums) {
+            if (num == 1) {
                 count++;
-                if(count > maxcount) {
+                if (count > maxcount) {
                     maxcount = count;
                 }
-            } else{
+            } else {
                 count = 0;
             }
         }
